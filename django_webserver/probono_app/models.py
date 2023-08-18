2921e9808da78f20a77df7490c9be2e373bea449
JeongHeumChoi <andyc707@gmail.com>
2023-08-19 06:17:18 +0900

    FEAT SpecialWeather

--- django_webserver/probono_app/models.py
+++ django_webserver/probono_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import requests
 
 # DB
 from bson.objectid import ObjectId
@@ -6,6 +7,10 @@ from bson.json_util import loads, dumps
 
 # User
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+# Special Weather
+from datetime import datetime
+from itertools import groupby
 
 # 이거 완전 쓸모 없어 형석아 지우자
 class CustomUserManager(BaseUserManager):
@@ -178,3 +183,70 @@ class demo():
     
     def __str__(self):
         return self.obj_id
+
+class SpecialWeather:
+    def __init__(self):
+        self.target_reg = [
+            ['L1100100', '서울동남권'],
+            ['L1100200', '서울동북권'],
+            ['L1100300', '서울서남권'],
+            ['L1100400', '서울서북권']
+        ]
+        self.wrn = {
+            'W': '강풍', 'R': '호우', 'C': '한파',
+            'D': '건조', 'O': '해일', 'N': '지진해일',
+            'V': '풍랑', 'T': '태풍', 'S': '대설',
+            'Y': '황사', 'H': '폭염'
+        }
+        self.lvl = {'1': '예비', '2': '주의보', '3': '경보'}
+
+    def init_special_weather(self, special_weathers):
+        key = 'm4y76-4OTnaMu-vuDg525w'
+        special_weathers.delete_many({})
+        to_insert = []
+        for target in self.target_reg:
+            content_str = self.fetch_data(target, key)
+            all_data = self.parse_data(content_str, target)
+            all_data.sort(key=lambda x: (x['WRN'], x['TM_EF']))
+            grouped_data = {key: list(group) for key, group in groupby(all_data, key=lambda x: x['WRN'])}
+            to_insert.extend(self.process_grouped_data(grouped_data, target))
+        special_weathers.insert_many(to_insert)
+
+    def fetch_data(self, target, key):
+        url = 'https://apihub.kma.go.kr/api/typ01/url/wrn_met_data.php'
+        params = {'wrn': 'A', 'reg': target[0], 'tmfc1': '202308010000', 'disp': '0', 'authKey': key}
+        response = requests.get(url, params=params)
+        return response.content.decode('utf-8')
+
+    def parse_data(self, content_str, target):
+        content_str = content_str.replace("#START7777", "").replace("#7777END", "").strip()
+        lines = content_str.split('\n')
+        all_data = []
+        for line in lines:
+            if line.startswith("#"):
+                continue
+            fields = line.split(',')
+            tm_ef = datetime.strptime(fields[1].strip(), "%Y%m%d%H%M")
+            data = {
+                'TM_EF': tm_ef,
+                'REG_NM': target[1],
+                'WRN': fields[5].strip(),
+                'LVL': fields[6].strip(),
+                'CMD': fields[7].strip(),
+            }
+            all_data.append(data)
+        return all_data
+
+    def process_grouped_data(self, grouped_data, target):
+        to_insert = []
+
+        for w, group in grouped_data.items():
+            if group[-1]['CMD'] != '3':
+                result = {
+                    'TM_EF': group[-1]['TM_EF'],
+                    'REG_NM': target[1],
+                    'WRN': self.wrn[w],
+                    'LVL': self.lvl[group[-1]['LVL']]
+                }
+                to_insert.append(result)
+        return to_insert
