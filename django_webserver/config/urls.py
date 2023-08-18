4b7716500d302b52afeb6f5dc31b6b27e4b21d94
JeongHeumChoi <andyc707@gmail.com>
2023-08-19 02:19:21 +0900

    FEAT get_special_weather

--- django_webserver/config/urls.py
+++ django_webserver/config/urls.py
@@ -34,5 +34,6 @@ urlpatterns = [
     path('get_subway_elvtr/', views.get_subway_elvtr, name='get_subway_elvtr'),
     path('get_safety_guard_house/',views.get_safety_guard_house,name='get_safety_guard_house'),
     path('get_bus_no_to_route/', views.get_bus_no_to_route, name='get_bus_no_to_route'),
-    path('get_bus_route/', views.get_bus_route, name='get_bus_route')
+    path('get_bus_route/', views.get_bus_route, name='get_bus_route'),
+    path('get_special_weather/', views.get_special_weather, name='get_special_weather')
 ]
