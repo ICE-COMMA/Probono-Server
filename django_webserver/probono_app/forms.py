9f37fda10e53a934378af87a9641a934633f2e88
JeongHeumChoi <andyc707@gmail.com>
2023-08-20 00:11:15 +0900

    FIX sign_up Form

--- django_webserver/probono_app/forms.py
+++ django_webserver/probono_app/forms.py
@@ -1,10 +1,9 @@
 from django import forms
-from .models import CustomUser
 
-class SignUpForm(forms.ModelForm):
-    class Meta:
-        model = CustomUser
-        fields = ['user_id', 'name', 'password', 'sex', 'birth', 'disability', 'custom']
-        widgets = {
-            'password': forms.PasswordInput(),
-        }
+class SignUpForm(forms.Form):
+    ID = forms.CharField(label="User ID", max_length=100, required=True)
+    name = forms.CharField(label="Name", max_length=100, required=True)
+    PW = forms.CharField(label="Password", widget=forms.PasswordInput(), required=True)
+    gender = forms.CharField(label="Gender", max_length=10, required=True)
+    date = forms.DateField(label="Date", required=True)
+    impaired = forms.CharField(label="Impaired", max_length=100, required=True)
