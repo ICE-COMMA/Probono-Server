8e6be82cc117bee0cd53cd673a05ceef9fa7d118
JeongHeumChoi <andyc707@gmail.com>
2023-08-21 05:35:43 +0900

    ADD

--- django_webserver/probono_app/static/js/edit.js
+++ django_webserver/probono_app/static/js/edit.js
@@ -1,44 +1,28 @@
-const cancel = document.querySelector("#profile_cancel");
 const edit = document.querySelector("#profile_complete");
 
-cancel.addEventListener("click", (e) => {
-  e.preventDefault();
-  window.location.href = "/";
-});
-
-document.querySelector("#profile").addEventListener("submit", async (e) => {
-  // return 정보 파악 후 ok이면
+edit.addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  function getCSRFToken() {
-    const csrfCookie = document.cookie.match(/csrftoken=([\w-]+)/);
-    if (csrfCookie) {
-      return csrfCookie[1];
-    }
-    return null;
-  }
-
-  const userID = document.querySelector("#user_id").value;
   const userHandicap = document.querySelector("#user_handicap").value;
   const previousPw = document.querySelector("#previous_pw").value;
   const nextPw = document.querySelector("#next_pw").value;
   const nextPwCheck = document.querySelector("#next_pw_check").value;
 
-  console.log(userID);
+  console.log(userHandicap);
 
   const requestBody = {
-    user_ID: userID,
     user_handicap: userHandicap,
     previous_pw: previousPw,
     next_pw: nextPw,
     next_pw_check: nextPwCheck,
   };
+
   try {
-    const response = await fetch(`/my_page/${userID}`, {
+    const response = await fetch("/my_page/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "X-CSRFToken": getCSRFToken(), // Replace with the actual CSRF token
+        "X-CSRFToken": csrf_token, // Replace with the actual CSRF token
       },
       body: JSON.stringify(requestBody),
     });
