94c780cd1d5f7fe715a9b77f9dc01d04905516cc
JeongHeumChoi <andyc707@gmail.com>
2023-08-31 17:12:32 +0900

    FIX

--- django_webserver/config/urls.py
+++ django_webserver/config/urls.py
@@ -36,5 +36,4 @@ urlpatterns = [
     path('get_bus_no_to_route/', views.get_bus_no_to_route, name='get_bus_no_to_route'),
     path('get_bus_route/<str:bus_num>', views.get_bus_route, name='get_bus_route'),
     path('get_demo_today/', views.get_demo_today, name='get_demo_today'),
-    path('get_hwp_text/',views.get_hwp_text,name='get_hwp_text')
 ]
