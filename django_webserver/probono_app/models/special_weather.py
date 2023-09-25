72450e2b5e2f63a3d80e6c97e6fb380f64444e36
JeongHeumChoi <andyc707@gmail.com>
2023-09-26 03:08:34 +0900

    REFACTOR special weather

--- django_webserver/probono_app/models/special_weather.py
+++ django_webserver/probono_app/models/special_weather.py
@@ -7,74 +7,94 @@ from itertools import groupby
 
 logger = logging.getLogger(__name__)
 
+from config import utils
+
+db_handle = utils.db_handle
+get_collection = utils.get_collection_handle
+
 class SpecialWeather():
     
     tmfc1_value = None
 
     def __init__(self):
-        self.base_url = 'https://apihub.kma.go.kr/api/typ01/url/wrn_met_data.php'
-        self.key = 'm4y76-4OTnaMu-vuDg525w'
+        self.base_url   = 'https://apihub.kma.go.kr/api/typ01/url/wrn_met_data.php'
+        self.key        = 'm4y76-4OTnaMu-vuDg525w'
+        self.params = {
+            'wrn'       : 'A',      'reg'   : None,
+            'tmfc1'     : None,     'disp'  : '0',
+            'authKey'   : self.key
+        }
+        self.db_name = 'special_weather'
         self.target_reg = [
             ['L1100100', '서울동남권'], ['L1100200', '서울동북권'],
             ['L1100300', '서울서남권'], ['L1100400', '서울서북권']
         ]
         self.wrn = {
-            'W': '강풍', 'R': '호우', 'C': '한파',
-            'D': '건조', 'O': '해일', 'N': '지진해일',
-            'V': '풍랑', 'T': '태풍', 'S': '대설',
-            'Y': '황사', 'H': '폭염'
+            'W' : '강풍', 'R'   : '호우', 'C'   : '한파',
+            'D' : '건조', 'O'   : '해일', 'N'   : '지진해일',
+            'V' : '풍랑', 'T'   : '태풍', 'S'   : '대설',
+            'Y' : '황사', 'H'   : '폭염'
         }
-        self.lvl = {'1': '예비', '2': '주의보', '3': '경보'}
+        self.lvl = {
+            '1' :   '예비',
+            '2' :   '주의보',
+            '3' :   '경보'
+        }
 
-    # main method
-    def init_special_weather(self, special_weathers):
+    def init_special_weather(self):
         print('Initializing Special Weather.. ', end='')
-        special_weathers.delete_many({})
-        to_insert = []
-        for target in self.target_reg:
-            ret_fetch_data = self.init_fetch_data(target)
-            if not ret_fetch_data[1]:
-                print('Error!')
-                return
-            content_str = ret_fetch_data[0]
-            # print(content_str)
-            all_data = self.parse_data(content_str, target)
-            all_data.sort(key=lambda x: (x['WRN'], x['TM_EF']))
-            grouped_data = {key: list(group) for key, group in groupby(
-                all_data, key=lambda x: x['WRN'])}
-            to_insert.extend(self.process_grouped_data(grouped_data, target))
+        collection = get_collection(db_handle, self.db_name)
+        collection.delete_many({})
+        to_insert = self._process_weather_data(self.init_fetch_data)
         if to_insert:
-            special_weathers.insert_many(to_insert)
+            collection.insert_many(to_insert)
         print('OK')
 
-    # main method
-    def update_special_weather(self, special_weathers):
+    def update_special_weather(self):
         print('Updating Special Weather.. ', end='')
-        new_data = []
+        collection = get_collection(db_handle, self.db_name)
+        new_data = self._process_weather_data(self.update_fetch_data)
+        for target in new_data:
+            target_db = collection.find_one(target['WRN'])
+            if not target_db:
+                collection.delete_one(target_db)
+                if target['CMD'] != '3':
+                    collection.insert_one(target)
+        print('OK')
+
+    def _process_weather_data(self, fetch_method):
+        processed_data = []
         for target in self.target_reg:
-            content_str = self.update_fetch_data(target)
+            ret_fetched_data = fetch_method(target)
+            if not ret_fetched_data[1]:
+                print('Error!')
+                return []
+            content_str = ret_fetched_data[0]
             all_data = self.parse_data(content_str, target)
             all_data.sort(key=lambda x: (x['WRN'], x['TM_EF']))
-            grouped_data = {key: list(group) for key, group in groupby(
-                all_data, key=lambda x: x['WRN'])}
-            new_data.extend(self.process_grouped_data(grouped_data, target))
+            grouped_data = {key: list(group) for key, group in groupby(all_data, key=lambda x: x['WRN'])}
+            processed_data.extend(self.process_grouped_data(grouped_data, target))
+        return processed_data
 
-        for target in new_data:
-            target_db = special_weathers.find_one(target['WRN'])
-            if not target_db:
-                special_weathers.delete_one(target_db)
-                if target['CMD'] != '3':
-                    special_weathers.insert_one(target)
-        print('OK')
 
     def init_fetch_data(self, target):
-        SpecialWeather.tmfc1_value = self.two_months_ago()
-        params = {
-            'wrn': 'A', 'reg': target[0],
-            'tmfc1': SpecialWeather.tmfc1_value,
-            'disp': '0', 'authKey': self.key
-        }
+        return self.fetch_data(target, is_initial=True)
+
+    def update_fetch_data(self, target):
+        return self.fetch_data(target)
+
+    def set_params(self, target):
+        self.params['reg'] = target[0]
+        self.params['tmfc1'] = SpecialWeather.tmfc1_value
+        return self.params
+
+    def fetch_data(self, target, is_initial=False):
+        if is_initial:
+            SpecialWeather.tmfc1_value = self.two_months_ago()
+            
+        params = self.set_params(target)
         SpecialWeather.tmfc1_value = datetime.now().strftime('%Y%m%d%H%M')
+        
         try:
             response = requests.get(self.base_url, params=params)
             response.raise_for_status()
@@ -91,28 +111,6 @@ class SpecialWeather():
             print(response)
         return response.content.decode('utf-8'), False
 
-    def update_fetch_data(self, target):
-        params = {
-            'wrn': 'A', 'reg': target[0],
-            'tmfc1': SpecialWeather.tmfc1_value,
-            'disp': '0', 'authKey': self.key
-        }
-        SpecialWeather.tmfc1_value = datetime.now().strftime('%Y%m%d%H%M')
-        try:
-            response = requests.get(self.base_url, params=params)
-            response.raise_for_status()
-            return response.content.decode('utf-8'), True
-        except requests.exceptions.HTTPError as http_err:
-            if response.status_code == 500:
-                print("Internal Server Error : Special weather API server.")
-            else:
-                print(f"HTTP Error : {http_err}")
-        except requests.exceptions.RequestException as req_err:
-            print(f"Request Error : {req_err}")
-        except Exception as e:
-            print(f"Error : {e}")
-            print(response)
-        return response.content.decode('utf-8'), False
 
     def parse_data(self, content_str, target):
         content_str = content_str.replace(
@@ -122,7 +120,6 @@ class SpecialWeather():
         for line in lines:
             if line.startswith("#"):
                 continue
-
             fields = line.split(',')
             if len(fields) < 2:
                 logger.error(f"Unexpected fields data: {fields}")
