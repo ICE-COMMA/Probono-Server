a48b04bb71413d65eaa948be93623645a8f1beca
JeongHeumChoi <andyc707@gmail.com>
2023-09-26 17:47:47 +0900

    REFACTOR class name rule

--- django_webserver/probono_app/models/__init__.py
+++ django_webserver/probono_app/models/__init__.py
@@ -1,7 +1,7 @@
 from .special_weather import SpecialWeather
 from .bus_info import BusInfo
 from .population_real_time import PopulationRealTime
-from .population_ai_model import district_info, PopulationAiModel
+from .population_ai_model import districtInfo, PopulationAiModel
 from .demo_scraper import DemoInfo
 from .customizing import CustomInfo
 from .subway_info import SubwayInfo
