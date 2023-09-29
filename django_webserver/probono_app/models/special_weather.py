841e17d96245e379ea90f8d4f3cc210c25586799
JeongHeumChoi <andyc707@gmail.com>
2023-09-29 20:06:48 +0900

    FIX db element

--- django_webserver/probono_app/models/special_weather.py
+++ django_webserver/probono_app/models/special_weather.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class SpecialWeather(models.Model):
-    TM_EF = models.DateTimeField()  # 효력 발휘 시각
+    TM_EF = models.CharField(max_length=50)  # 효력 발휘 시각
     REG_NM = models.CharField(max_length=100)  # 지역 이름 (예: 서울동남권)
     WRN = models.CharField(max_length=100)  # 특보 종류 (예: 강풍, 호우 등)
     LVL = models.CharField(max_length=50)  # 특보 레벨 (예: 예비, 주의보, 경보)
--- django_webserver/probono_app/models/user.py
+++ django_webserver/probono_app/models/user.py
@@ -16,7 +16,7 @@ class User(models.Model):
     PW = models.CharField(max_length=50) # In practice, never save plain passwords. Use Django's User model & authentication system.
     gender = models.CharField(max_length=10, choices=[('male', 'Male'), ('female', 'Female')], null=True, blank=True)
     date = models.DateField(null=True, blank=True) # Birthdate maybe?
-    impaired = models.CharField(max_length=100, null=True, blank=True) # Assuming it's a string, but not sure of its content
+    impaired = models.CharField(max_length=50, null=True, blank=True) # Assuming it's a string, but not sure of its content
     custom = models.OneToOneField(CustomPreferences, on_delete=models.CASCADE)
 
     class Meta:
