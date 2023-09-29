beb125da32865d8ad1f1c0af2d3ed6de9e4c6e85
JeongHeumChoi <andyc707@gmail.com>
2023-09-30 01:51:42 +0900

    FIX int to char field

--- django_webserver/probono_app/models/demo.py
+++ django_webserver/probono_app/models/demo.py
@@ -4,7 +4,7 @@ class Demo(models.Model):
     location = models.CharField(max_length=255)
     date = models.DateField()
     time = models.CharField(max_length=255)  # 시간 범위를 문자열로 표시
-    amount = models.PositiveIntegerField()
+    amount = models.CharField(max_length=8)
 
     class Meta:
         db_table = 'demo'
