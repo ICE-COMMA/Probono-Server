6da12c492dc5409825c04dbbd199e9135f8246f0
JeongHeumChoi <andyc707@gmail.com>
2023-09-30 19:19:59 +0900

    REFACTOR

--- django_webserver/probono_app/models/special_weather.py
+++ django_webserver/probono_app/models/special_weather.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class SpecialWeather(models.Model):
-    TM_EF   = models.CharField(max_length=50)  # 효력 발휘 시각
-    REG_NM  = models.CharField(max_length=100)  # 지역 이름 (예: 서울동남권)
-    WRN     = models.CharField(max_length=100)  # 특보 종류 (예: 강풍, 호우 등)
-    LVL     = models.CharField(max_length=50)  # 특보 레벨 (예: 예비, 주의보, 경보)
+    TM_EF   = models.CharField(max_length=50)
+    REG_NM  = models.CharField(max_length=100)
+    WRN     = models.CharField(max_length=100)
+    LVL     = models.CharField(max_length=50)
 
     class Meta:
         db_table = 'special_weather'
