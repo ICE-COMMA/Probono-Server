9a51a3ac905cb55cc3325257d22cf6e4926cc285
JeongHeumChoi <andyc707@gmail.com>
2023-10-12 20:28:40 +0900

    FEAT SafetyGuardHouseInfo

---
+++ django_webserver/probono_app/services/safety_guard_house_info.py
@@ -0,0 +1,45 @@
+import requests
+
+from probono_app.models import SafetyGuardHouse
+
+class SafetyGuardHouseInfo():
+
+    def __init__(self):
+        self.__key = 'z3tbVitFT7XffZ43RQ9sMyE0ALiv+EtqOysMUKPdg9E5zTIL3lNVHqGCOS9vPqq73zYw6OhwHiskVZj4MYCJ0w=='
+        self.__base_url = "http://api.data.go.kr/openapi/tn_pubr_public_female_safety_prtchouse_api"
+
+    def get_safety_guard_house(self):
+        start_index = 1
+        end_index = 100
+
+        SafetyGuardHouse.objects.all().delete()
+
+        to_insert = []
+        while True:
+            params = {
+                'serviceKey'    : self.__key,
+                'pageNo'        : start_index,
+                'numOfRows'     : end_index,
+                'type'          : 'json'
+            }
+            response = requests.get(self.__base_url, params=params)
+            data = response.json()
+
+            if 'response' in data and 'body' in data['response'] and 'items' in data['response']['body']:
+                items = data['response']['body']['items']
+                for target in items:
+                    if target['ctprvnNm'] == '서울특별시':
+                        name = target.get('storNm', '')
+                        x = target.get('longitude', '')
+                        y = target.get('latitude', '')
+                        safety_guard_house = SafetyGuardHouse(name=name, x=float(x), y=float(y))
+                        to_insert.append(safety_guard_house)
+                if len(items) < 100:
+                    break
+            else:
+                break
+            start_index += 1
+        # print(to_insert)
+        # print(len(to_insert))
+        SafetyGuardHouse.objects.bulk_create(to_insert)
+        return
